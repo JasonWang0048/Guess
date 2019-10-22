c09f73d9a55383d1d927987c65eac01f7db3d7e1
miia <miia@com.tw>
2019-10-22 17:14:10 +0800

    second commit

--- app/src/main/java/com/tom/guess/MainActivity.java
+++ app/src/main/java/com/tom/guess/MainActivity.java
@@ -8,18 +8,33 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText edGuess;
+    int secret = new Random().nextInt(10) + 1;
+    String TAG = MainActivity.class.getSimpleName();
+    private int num;
+    private TextView react;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "secret: " + secret);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        edGuess = findViewById(R.id.guessing);
+        react = findViewById(R.id.reaction);
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -29,6 +44,23 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    public void guessNumber(View view) {
+        num = Integer.parseInt(edGuess.getText().toString());
+        if (num == secret) {
+            react.setText("That is Right!!");
+            Toast.makeText(MainActivity.this, "You're smart!", Toast.LENGTH_LONG).show();
+        } else {
+            react.setText("Wrong! Try again~");
+            if (num < secret) {
+                Toast.makeText(MainActivity.this, "Guess a bigger one.", Toast.LENGTH_LONG).show();
+            } else if (num > secret) {
+                Toast.makeText(MainActivity.this, "Guess a smaller one.", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(MainActivity.this, "Please enter a number!!!", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 
     @Override
